05519b87a16305fd07a0b4fddc096433cb540291
M0mahmoud <houdmohamed85@gmail.com>
2022-09-08 16:43:06 +0200

    File Uploads

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/models/user.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/models/user.js
@@ -52,6 +52,9 @@ const userSchema = new mongoose.Schema(
         },
       },
     ],
+    avatar:{
+      type:Buffer
+    }
   },
   {
     timestamps: true,
@@ -70,6 +73,7 @@ userSchema.methods.toJSON = function () {
   const userObj = user.toObject();
   delete userObj.password;
   delete userObj.tokens;
+  delete userObj.avatar;
   return userObj;
 };
 
